00cab977797cb5447965ea6097eb53a6ade8cef5
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-05-30 08:31:10 +0530

    Test variables refactored
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- test/test_splitwebp.cpp
+++ test/test_splitwebp.cpp
@@ -2,32 +2,32 @@
 #include "splitwebp.h"
 
 TEST(splitwebp, existsInputFile_invalid) {
-    splitwebp::SplitWebP swp = splitwebp::SplitWebP("/dev/$$$/^^^");
-    ASSERT_TRUE(!swp.existsInputFile());
+    splitwebp::SplitWebP splitWebP = splitwebp::SplitWebP("/dev/$$$/^^^");
+    ASSERT_TRUE(!splitWebP.existsInputFile());
 }
 
 TEST(splitwebp, setInputFile_normal) {
-    splitwebp::SplitWebP swp = splitwebp::SplitWebP("__default");
-    ASSERT_TRUE(swp.setInputFile("test"));
-    ASSERT_EQ(swp.getInputFileName(), "test");
+    splitwebp::SplitWebP splitWebP = splitwebp::SplitWebP("__default");
+    ASSERT_TRUE(splitWebP.setInputFile("test"));
+    ASSERT_EQ(splitWebP.getInputFileName(), "test");
 }
 
 TEST(splitwebp, setInputFile_empty) {
-    splitwebp::SplitWebP swp = splitwebp::SplitWebP("__default");
+    splitwebp::SplitWebP splitWebP = splitwebp::SplitWebP("__default");
 
     testing::internal::CaptureStderr();
-    ASSERT_TRUE(!swp.setInputFile(""));
-    ASSERT_EQ(swp.getInputFileName(), "");
+    ASSERT_TRUE(!splitWebP.setInputFile(""));
+    ASSERT_EQ(splitWebP.getInputFileName(), "");
 
     ASSERT_EQ(testing::internal::GetCapturedStderr(), 
         "[ERROR : Input File Name is null ]\n");
 }
 
 TEST(splitwebp, load_invalid) {
-    splitwebp::SplitWebP swp = splitwebp::SplitWebP("/dev/$$$/^^^");
+    splitwebp::SplitWebP splitWebP = splitwebp::SplitWebP("/dev/$$$/^^^");
     
     testing::internal::CaptureStderr();
-    swp.load();
+    splitWebP.load();
     ASSERT_EQ(testing::internal::GetCapturedStderr(), 
         "[ERROR : Input file does not exist. ]\n");
 }
